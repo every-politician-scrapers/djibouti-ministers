d52c82173641f759f6be3d1bf815887fd628e40e
Tony Bowden <tony@tmtm.com>
2022-07-16 11:40:09 +0300

    upgrade scraper

--- sources/official/add-p39.js
+++ sources/official/add-p39.js
@@ -2,13 +2,11 @@ const fs = require('fs');
 let rawmeta = fs.readFileSync('meta.json');
 let meta = JSON.parse(rawmeta);
 
-module.exports = (id, position, startdate, enddate) => {
-  qualifier = {
-    P5054: meta.cabinet.id,
-  }
-
-  if(startdate) qualifier['P580']  = startdate
-  if(enddate)   qualifier['P582']  = enddate
+module.exports = (id, label, position, positionlabel, startdate, enddate) => {
+  qualifier = { }
+  if(startdate)              qualifier['P580']  = startdate
+  if(enddate)                qualifier['P582']  = enddate
+  if(!startdate && !enddate) qualifier['P5054'] = meta.cabinet.id
 
   return {
     id,
@@ -16,6 +14,12 @@ module.exports = (id, position, startdate, enddate) => {
       P39: {
         value: position,
         qualifiers: qualifier,
+        references: {
+          ...meta.reference,
+          P813: new Date().toISOString().split('T')[0],
+          P1810: label,
+          P1932: positionlabel,
+        }
       }
     }
   }
